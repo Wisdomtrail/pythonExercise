e304c183e1e03ae478d0ef7e778663d653bebef0
sunepa01 <117924550+sunepa01@users.noreply.github.com>
2022-12-05 11:18:04 +0100

    first commit

---
+++ output.py
@@ -0,0 +1,33 @@
+import random
+
+month = random.randrange(1, 12)
+if month == 1:
+    print("january")
+elif month == 2:
+    print("february")
+elif month == 3:
+    print("march")
+elif month == 4:
+    print("April")
+elif month == 5:
+    print(" May")
+elif month == 6:
+    print("june")
+elif month == 7:
+    print("July")
+elif month == 8:
+    print("August")
+elif month == 9:
+    print("September")
+elif month == 10:
+    print("October")
+elif month == 11:
+    print("November")
+else:
+    print("December")
+
+
+
+
+
+
